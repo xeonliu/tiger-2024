ea9e7383182835875628ac930820da28f80e1210
Your Name <you@example.com>
2025-01-11 12:24:12 +0000

    First Try

--- src/tiger/regalloc/regalloc.cc
+++ src/tiger/regalloc/regalloc.cc
@@ -1,12 +1,500 @@
 #include "tiger/regalloc/regalloc.h"
 
+#include "tiger/frame/frame.h"
+#include "tiger/frame/temp.h"
+#include "tiger/liveness/flowgraph.h"
+#include "tiger/liveness/liveness.h"
 #include "tiger/output/logger.h"
+#include "tiger/regalloc/color.h"
 #include <iostream>
+#include <memory>
 
 extern frame::RegManager *reg_manager;
+// 可以用于获取Function Frame信息
 extern std::map<std::string, std::pair<int, int>> frame_info_map;
 
 namespace ra {
 /* TODO: Put your lab6 code here */
+RegAllocator::RegAllocator(const std::string &proc_name,
+                           std::unique_ptr<cg::AssemInstr> assem_instr)
+    : proc_name_(proc_name), assem_instr_(std::move(assem_instr)) {}
+
+void RegAllocator::RegAlloc() {
+  // procedure Main()
+  // LivenessAnalysis()
+  // Build()
+
+  // MakeWorklist()
+  // repeat
+  //     if simplifyWorklist != {} then Simplify()
+  //     else if worklistMoves != {} then Coalesce()
+  //     else if freezeWorklist != {} then Freeze()
+  //     else if spillWorklist != {} then SelectSpill()
+  // until simplifyWorklist = {} && worklistMove={} &&
+  //          freezeWorklist={} && spillWorklist={}
+  // AssignColor()
+  // if spilledNodes != {} then
+  //     RewriteProgram(spilledNodes)
+  //     Main()
+
+  // Build a FlowGraph
+
+  fg::FlowGraphFactory flowgraph_factory(assem_instr_->GetInstrList());
+  flowgraph_factory.AssemFlowGraph();
+  fg::FGraphPtr flowgraph = flowgraph_factory.GetFlowGraph();
+
+  // Liveness Analysis
+  live::LiveGraphFactory live_graph_factory(flowgraph);
+  live_graph_factory.Liveness();
+  live::LiveGraph live_graph = live_graph_factory.GetLiveGraph();
+
+  // Build Interference Graph
+  // 从Live Graph中初始化
+  interference_graph_ = live_graph.interf_graph;
+  moves_ = live_graph.moves;
+
+  Build();
+  MakeWorklist();
+  while (!simplify_worklist_->GetList().empty() ||
+         !worklist_moves_->GetList().empty() ||
+         !freeze_worklist_->GetList().empty() ||
+         !spill_worklist_->GetList().empty()) {
+    if (!simplify_worklist_->GetList().empty()) {
+      Simplify();
+    } else if (!worklist_moves_->GetList().empty()) {
+      Coalesce();
+    } else if (!freeze_worklist_->GetList().empty()) {
+      Freeze();
+    } else if (!spill_worklist_->GetList().empty()) {
+      SelectSpill();
+    }
+  }
+  AssignColors();
+
+  if (!spilled_nodes->Empty()) {
+    RewriteProgram();
+    RegAlloc();
+  } else {
+
+    // Create a temp::Map coloring map
+    auto coloring = temp::Map::Empty();
+    for (auto node : colored_nodes_->GetList()) {
+      // TODO: Coloring 是个什么东西？
+      coloring->Enter(node->NodeInfo(),
+                      reg_manager->GetRegister(color_.at(node)));
+    }
+    // Prepare the instruction list
+    result_ = std::make_unique<Result>(coloring_result.coloring,
+                                       Strip(coloring_result.coloring));
+    temp::Map *color =
+        temp::Map::LayerMap(reg_manager->temp_map_, coloring_result.coloring);
+  }
+
+  // FIXME
+  // 需要初始化的变量
+  // active_moves_: 是虚线边，就是初始的集合
+  // move_list_：是实线边，是node和move的映射
+  // 把机器寄存器的Color上色
+  // precolored_
+  // 机器寄存器的Temp号是内定的，从0x100开始
+  // initial_
+  // degree_
+  // adj_list
+  // adj_set
+}
+
+/**!SECTION
+ * Build Interference Graph
+ *
+ * moveList
+ * workListMoves
+ * AddEdge
+ */
+void RegAllocator::Build() {
+  // Use Information Obtained From Liveness Analysis to initialize the data
+  // structures
+
+  // RegManager contains all temporaries for registers
+  temp::Map *temp_map = reg_manager->temp_map_;
+
+  for (live::INodePtr node : interference_graph_->Nodes()->GetList()) {
+    // Initialize the degree of each node
+    degree_->Enter(node, new int(node->OutDegree()));
+
+    // Initiallize the moveList
+    live::MoveList *node_move_list = new live::MoveList();
+    for (auto move : worklist_moves_->GetList()) {
+      // FIXME: 有可能合并的传送指令集合？
+      auto src = move.first;
+      auto dst = move.second;
+      if (src == node || src == node) {
+        node_move_list->Append(src, dst);
+      }
+    }
+    move_list_->Enter(node, node_move_list);
+
+    // Initialize precolored & initial
+    if (temp_map->Look(node->NodeInfo())) {
+      precolored_->Append(node);
+    } else {
+      initial_->Append(node);
+    }
+  }
+}
+
+/**
+ * @brief  将节点从initial中移动到各个Worklist中
+ * @note
+ * @retval None
+ */
+void RegAllocator::MakeWorklist() {
+  // 初始化工作列表，将干涉图中的节点分类到不同的工作列表中，根据节点的度数（Degree，表示与其他节点的连接数量）以及其他特性分配到以下几类工作列表：
+  // simplifyWorklist：低度数节点，可以直接简化。
+  // worklistMoves：涉及变量移动的节点。
+  // freezeWorklist：需要冻结的节点。
+  // spillWorklist：高风险会导致溢出的节点。
+  for (live::INodePtr node : interference_graph_->Nodes()->GetList()) {
+    initial_->DeleteNode(node);
+    if (node->Degree() >= K_) {
+      spill_worklist_->Append(node);
+    } else if (MoveRelated(node)) {
+      freeze_worklist_->Append(node);
+    } else {
+      simplify_worklist_->Append(node);
+    }
+  }
+}
+
+live::INodeListPtr RegAllocator::Adjacent(live::INodePtr node) {
+  // TODO
+  return nullptr;
+}
+
+live::MoveList *RegAllocator::NodeMoves(live::INodePtr node) {
+  // TODO
+  return move_list_->Look(node)->Intersect(
+      active_moves_->Union(worklist_moves_));
+}
+
+bool RegAllocator::MoveRelated(live::INodePtr node) {
+  return NodeMoves(node)->GetList().size() > 0;
+}
+
+/**
+ * 将simplofyWorklist中的节点简化
+ * 内容压入selectStack
+ * 将相邻边节点度数降低
+ */
+void RegAllocator::Simplify() {
+  if (simplify_worklist_->GetList().empty()) {
+    return;
+  }
+  // 从simplifyWorklist中取出一个节点，将其压入selectStack中
+  live::INodePtr node = simplify_worklist_->GetList().front();
+  simplify_worklist_->DeleteNode(node);
+  select_stack_->Prepend(node);
+
+  // 将node的相邻节点的度数减1
+  for (live::INodePtr adj_node : Adjacent(node)->GetList()) {
+    DecrementDegree(adj_node);
+  }
+}
+
+void RegAllocator::DecrementDegree(live::INodePtr m) {
+  int *d = degree_->Look(m);
+  // 降度数
+  --(*d);
+  if (*d == K_ - 1) {
+    // 当邻节点的度数从K变到K-1时，
+    // 他的领结点相关的传送指令将有可能变成可合并的
+    // 将其从spillWorklist中移动到simplifyWorklist中
+    live::INodeListPtr m_set = new live::INodeList();
+    m_set->Append(m);
+    EnableMoves(m_set->Union(Adjacent(m)));
+    spill_worklist_->DeleteNode(m);
+    if (MoveRelated(m)) {
+      // 低度数传送有关节点表
+      freeze_worklist_->Union(m_set);
+    } else {
+      // 低度数传送无关节点表
+      simplify_worklist_->Union(m_set);
+    }
+  }
+}
+
+void RegAllocator::EnableMoves(live::INodeListPtr nodes) {
+  for (live::INodePtr node : nodes->GetList()) {
+    for (auto move : NodeMoves(node)->GetList()) {
+      // 与node相关的传送指令
+      // 全部变为做好准备的(加入worklistMoves)
+      if (active_moves_->Contain(move.first, move.second)) {
+        // 将activeMoves变为worklistMoves
+        active_moves_->Delete(move.first, move.second);
+        // TODO: Union?
+        worklist_moves_->Append(move.first, move.second);
+      }
+    }
+  }
+}
+
+void RegAllocator::Coalesce() {
+  // 从worklistMoves中取出一个传送指令
+  // 如果传送指令的源节点和目标节点都在同一个集合中
+  // 则将传送指令合并
+  // 否则，将传送指令加入冻结列表
+  if (worklist_moves_->GetList().empty()) {
+    return;
+  }
+  live::INodePtr x, y;
+  std::tie(x, y) = worklist_moves_->GetList().front();
+
+  live::INodePtr x_alias = GetAlias(x);
+  live::INodePtr y_alias = GetAlias(y);
+
+  live::INodePtr u = x_alias;
+  live::INodePtr v = y_alias;
+
+  if (precolored_->Contain(y_alias)) {
+    std::swap(u, v);
+  }
+
+  // 确保v是机器寄存器当且仅当二者都是机器寄存器
+  worklist_moves_->Delete(x, y);
+
+  if (u == v) {
+    // 合并传送指令
+    coalesced_moves_->Union(x, y);
+    AddWorkList(u);
+    return;
+  }
+
+  if (precolored_->Contain(v) || u->Adj(v)) {
+    // 如果二者冲突（即都是寄存器或者是相互冲突的临时变量）
+    // 冻结传送指令
+    constrained_moves_->Union(u, v);
+    AddWorkList(u);
+    AddWorkList(v);
+    return;
+  }
+
+  if (precolored_->Contain(u)) {
+    // 对于机器寄存器和临时变量的情况
+    // 采取Georage方法
+    // 对任意属于Adjacent(v)的节点t, Ok(t,u)
+    auto adj_v = Adjacent(v);
+    bool ok = true;
+    for (live::INodePtr t : adj_v->GetList()) {
+      if (!Ok(t, u)) {
+        ok = false;
+        break;
+      }
+    }
+    // 进行合并
+    if (ok) {
+      coalesced_moves_->Union(x, y);
+      Combine(u, v);
+      AddWorkList(u);
+      return;
+    }
+    // 不OK，放回没准备好的传送指令
+  } else {
+    // 对于两个临时变量的情况
+    // 采取Briggs方法
+    if (Conservative(Adjacent(u)->Union(Adjacent(v)))) {
+      coalesced_moves_->Union(x, y);
+      Combine(u, v);
+      AddWorkList(u);
+      return;
+    }
+  }
+
+  active_moves_->Union(u, v);
+}
+
+bool RegAllocator::Ok(live::INodePtr t, live::INodePtr u) {
+  return t->Degree() < K_ || precolored_->Contain(t) || t->Adj(u);
+}
+
+bool RegAllocator::Conservative(live::INodeListPtr nodes) {
+  int k = 0;
+  for (live::INodePtr node : nodes->GetList()) {
+    if (node->Degree() >= K_) {
+      ++k;
+    }
+  }
+  return k < K_;
+}
+
+live::INodePtr RegAllocator::GetAlias(live::INodePtr n) {
+  if (coalesced_nodes_->Contain(n)) {
+    return GetAlias(alias_->Look(n));
+  }
+  return n;
+}
+
+void RegAllocator::Combine(live::INodePtr u, live::INodePtr v) {
+  if (freeze_worklist_->Contain(v)) {
+    freeze_worklist_->DeleteNode(v);
+  } else {
+    spill_worklist_->DeleteNode(v);
+  }
+  coalesced_nodes_->Union(v);
+  alias_->Enter(v, u);
+  move_list_->Union(u, v);
+  for (live::INodePtr t : Adjacent(v)->GetList()) {
+    AddEdge(t, u);
+    DecrementDegree(t);
+  }
+  if (u->Degree() >= K_ && freeze_worklist_->Contain(u)) {
+    freeze_worklist_->DeleteNode(u);
+    spill_worklist_->Union(u);
+  }
+}
+
+/**
+ * @brief 将相关的传送指令冻结，重新从free_worklist送入simplify_worklist
+ * @note
+ * @retval None
+ */
+void RegAllocator::Freeze() {
+  if (freeze_worklist_->GetList().empty()) {
+    return;
+  }
+  live::INodePtr u = freeze_worklist_->GetList().front();
+  freeze_worklist_->DeleteNode(u);
+  simplify_worklist_->Union(u);
+  FreezeMoves(u);
+}
+
+void RegAllocator::FreezeMoves(live::INodePtr u) {
+  for (auto move : NodeMoves(u)->GetList()) {
+    live::INodePtr x = move.first;
+    live::INodePtr y = move.second;
+    live::INodePtr v;
+    if (GetAlias(y) == GetAlias(u)) {
+      v = GetAlias(x);
+    } else {
+      v = GetAlias(y);
+    }
+    active_moves_->Delete(x, y);
+    frozen_moves_->Union(x, y);
+    if (NodeMoves(v)->GetList().empty() && v->Degree() < K_) {
+      freeze_worklist_->DeleteNode(v);
+      simplify_worklist_->Append(v);
+    }
+  }
+}
+
+/**
+ * @brief
+ * 如果没有低度数的节点，选择一个潜在可能溢出的高度数节点并将他压入栈（进行简化）
+ * @note
+ * @retval None
+ */
+void RegAllocator::SelectSpill() {
+  // 选择一个溢出风险最高的节点
+  // 将其从spillWorklist中移动到simplifyWorklist中
+  live::INodePtr m = spill_worklist_->GetList().front();
+  spill_worklist_->DeleteNode(m);
+  simplify_worklist_->Append(m);
+  FreezeMoves(m);
+}
+
+col::Result RegAllocator::AssignColors() {
+  while (!select_stack_->GetList().empty()) {
+    // n = pop(select_stack)
+    live::INodePtr n = select_stack_->GetList().front();
+    select_stack_->DeleteNode(n);
+
+    std::set<int> ok_colors;
+    for (int i = 0; i < K_; ++i) {
+      ok_colors.insert(i);
+    }
+
+    for (live::INodePtr w : Adjacent(n)->GetList()) {
+      live::INodePtr alias_w = GetAlias(w);
+      if (colored_nodes_->Contain(alias_w) || precolored_->Contain(alias_w)) {
+        ok_colors.erase(color_.at(alias_w));
+      }
+    }
+
+    if (ok_colors.empty()) {
+      spilled_nodes_->Union(n);
+    } else {
+      colored_nodes_->Union(n);
+      color_.insert({n, *ok_colors.begin()});
+    }
+
+    for (auto n : coalesced_nodes_->GetList()) {
+      color_.insert({n, color_.at(GetAlias(n))});
+    }
+  }
+}
+
+/**
+ * @brief
+ * @note
+ * @retval None
+ */
+void RegAllocator::RewriteProgram() {
+  // 关于Frame
+  // frame_info_map: 函数名 -> {Offset，FrameSize}
+  // Frame Size 需要修改？
+
+  // 为了溢出的临时变量分配栈空间
+  // FIXME: How do I get the function names?????
+  int offset = frame_info_map[proc_name_].second;
+  int local_frame_size = frame_info_map[proc_name_].second;
+  frame_info_map[proc_name_].second += spilled_nodes_->GetList().size() * 16;
+
+  live::INodeListPtr new_temps = new live::INodeList();
+  // 为每一个v \in spilledNodes分配一个储存单元
+  for (live::INodePtr v : spilled_nodes_->GetList()) {
+    // 创建一个新的temp，把这条指令中的temp替换为新的temp
+    temp::Temp *new_temp = temp::TempFactory::NewTemp();
+
+    // 遍历所有指令
+    auto instr_iter = assem_instr_->GetInstrList()->GetList().begin();
+    while (instr_iter != assem_instr_->GetInstrList()->GetList().end()) {
+      auto instr = *instr_iter;
+      if (instr->Use() && instr->Use()->Contain(v->NodeInfo())) {
+        // v的每一个使用之前插入一条取数指令
+        instr->Use()->Replace(v->NodeInfo(), new_temp);
+        auto fetchInstr = new assem::OperInstr(
+            "movq (" + proc_name_ + "_frame_size_local-" +
+                std::to_string(offset) + ")(%rsp), `d0",
+            new temp::TempList({new_temp}),
+            new temp::TempList{reg_manager->FramePointer()}, nullptr);
+        assem_instr_->GetInstrList()->Insert(instr_iter, fetchInstr);
+      }
+      if (instr->Def() && instr->Def()->Contain(v->NodeInfo())) {
+        // v的每一个定值之后插入一条储存指令
+        instr->Def()->Replace(v->NodeInfo(), new_temp);
+        auto storeInstr = new assem::OperInstr(
+            "movq `s0, (" + proc_name_ + "_frame_size_local-" +
+                std::to_string(offset) + ")(%rsp)",
+            nullptr, new temp::TempList({reg_manager->FramePointer()}),
+            nullptr);
+        assem_instr_->GetInstrList()->Insert(++instr_iter, storeInstr);
+      }
+      ++instr_iter;
+    }
+    // FIXME: WTF? What if in different frames?
+    offset += 8;
+
+    // 将所有的v_i放入newTemps中
+    live::INodePtr new_node = interference_graph_->NewNode(new_temp);
+    new_temps->Append(new_node);
+  }
+
+  // spilledNoeds <- {}
+  // initial <- coloredNodes U coalescedNodes U newTemps
+  // coloredNodes <- {}
+  // coalescedNodes <- {}
+  spilled_nodes_->Clear();
+  initial_ = colored_nodes_->Union(coalesced_nodes_->Union(new_temps));
+  colored_nodes_->Clear();
+  coalesced_nodes_->Clear();
+}
 
 } // namespace ra
--- src/tiger/regalloc/regalloc.h
+++ src/tiger/regalloc/regalloc.h
@@ -8,8 +8,35 @@
 #include "tiger/liveness/liveness.h"
 #include "tiger/regalloc/color.h"
 #include "tiger/util/graph.h"
+#include <memory>
 
 namespace ra {
+
+/**
+ * Register Allocation
+ * Takes a flow graph to produce a live::LiveGraph
+ * An interference graph
+ * Using liveness information at the exit of each node
+ * A list of node-pairs representing MOVE instructions
+ */
+
+/** Two ways to represent edges
+Adjacent list
+Only for normal temporaries
+Because the adjacent list for machine registers may be too large
+Machine registers cannot be selected
+We only coalesce temporary nodes to machine registers using George method
+Bitmap
+Both for temporaries and machine registers
+Move-related node
+associated with a count number of moves
+easy to maintain and test if it is no longer move-related
+* All nodes
+Associated with a count of the number of neighbors currently in the graph
+This number is used to determine whether
+a node is of significant degree during coalescing
+a node can be removed from the graph during simplification
+ */
 
 class Result {
 public:
@@ -26,9 +53,143 @@ public:
   ~Result();
 };
 
+/**
+ * @brief  Build
+Simplify
+Remove non-move-related nodes of low-degree
+Coalesce
+Resulting node may become non-move-related node
+Freeze
+Freeze the moves node of low-degree
+Potential Spill
+Select
+If failed, rewrite code to implement actual spill and rebuild the RIG
+
+ * @note
+ * @retval None
+ */
 class RegAllocator {
   /* TODO: Put your lab6 code here */
+  // LINK - src/tiger/output/output.cc
+  //  ra::RegAllocator reg_allocator(body_->getName().str(),
+  //                                 std::move(assem_instr));
+  //  reg_allocator.RegAlloc();
+  //  allocation = reg_allocator.TransferResult();
+  //  il = allocation->il_;
+  //  color = temp::Map::LayerMap(reg_manager->temp_map_,
+  //  allocation->coloring_);
+public:
+  /**
+   * @brief
+   * @note
+   * @param  &proc_name:
+   * @param  assem_instr: Lab5 生成的汇编指令
+   * @retval
+   */
+  RegAllocator(const std::string &proc_name,
+               std::unique_ptr<cg::AssemInstr> assem_instr);
 
+  void RegAlloc();
+
+  std::unique_ptr<Result> TransferResult() { return std::move(result_); };
+
+  std::unique_ptr<Result> result_;
+
+private:
+  void Build();
+  void MakeWorklist();
+  live::INodeListPtr Adjacent(live::INodePtr node);
+  live::MoveList *NodeMoves(live::INodePtr node);
+  bool MoveRelated(live::INodePtr node);
+  void Simplify();
+  void DecrementDegree(live::INodePtr m);
+  void EnableMoves(live::INodeListPtr nodes);
+  void Coalesce();
+  void AddWorkList(live::INodePtr u);
+  bool Ok(live::INodePtr t, live::INodePtr u);
+  bool Conservative(live::INodeListPtr nodes);
+  live::INodePtr GetAlias(live::INodePtr n);
+  void Combine(live::INodePtr u, live::INodePtr v);
+  void Freeze();
+  void FreezeMoves(live::INodePtr u);
+  void SelectSpill();
+  col::Result AssignColors();
+  void RewriteProgram();
+
+  std::string proc_name_;
+  std::unique_ptr<cg::AssemInstr> assem_instr_;
+  int K_;
+
+  // Obtained From Liveness Analysis
+  // FIXME: Adjacent list: Only for normal temporaries??
+  live::IGraphPtr interference_graph_;
+  live::MoveList *moves_;
+
+  // 机器寄存器集合
+  live::INodeListPtr precolored_;
+
+  // P41
+  // initial: Temporary registers, not precolored and not yet processed
+  // 临时寄存器集合
+  live::INodeListPtr initial_;
+
+  /* Data Structures for Codegen */
+  // P39: A mapping from a node to the list of moves it is associated with
+  std::unique_ptr<tab::Table<live::INode, live::MoveList>> move_list_;
+
+  // FIXME: P40
+  // adjSet: The bit-matrix of interference graph
+  // adjList: The adjacent nodes list of interference graph
+  /**
+   * @brief
+        All nodes
+        Associated with a count of the number of neighbors currently in the
+   graph This number is used to determine whether a node is of significant
+   degree during coalescing a node can be removed from the graph during
+   simplification
+   */
+  // An array containing the current degree of each node
+  std::unique_ptr<tab::Table<live::INode, int>> degree_;
+
+  // alias.并查集
+  std::unique_ptr<tab::Table<live::INode, live::INode>> alias_;
+
+  // color.颜色表
+  // 从中获取染色结果
+  std::map<live::INodePtr, int> color_;
+
+  // P37
+  // low-degree non-move-related nodes
+  live::INodeListPtr simplify_worklist_;
+  // low-degree move-related nodes
+  live::INodeListPtr freeze_worklist_;
+  // high-degree nodes
+  live::INodeListPtr spill_worklist_;
+
+  // nodes marked for spilling
+  live::INodeListPtr spilled_nodes_;
+  // 已合并的寄存器集合
+  // 当合并u<-v时，将加入到这个集合中，u则被放回到某个工作表中（或反之）
+  live::INodeListPtr coalesced_nodes_;
+  live::INodeListPtr colored_nodes_;
+
+  // P176 存放move指令的集合
+  // 一共五个，所有的move指令在同一时刻分布在不同的集合中，互不重叠
+  // 记录move指令是否在Coalesce过程中被处理过
+  // 1. 已经合并的move指令
+  live::MoveList *coalesced_moves_;
+  // 2. src和dst存在冲突的move指令
+  live::MoveList *constrained_moves_;
+  // 3. 不再考虑合并的move指令
+  live::MoveList *frozen_moves_;
+  // 4. 有可能合并的move指令（等待在Colaesce()中处理）
+  live::MoveList *worklist_moves_;
+  // 5.
+  // 尚未做好准备进行合并的move指令（Colasece()不处理这些，在EnableMoves()中启用）
+  live::MoveList *active_moves_;
+
+  // 一个包含从图中删除的临时变量的栈
+  live::INodeListPtr select_stack_;
 };
 
 } // namespace ra
